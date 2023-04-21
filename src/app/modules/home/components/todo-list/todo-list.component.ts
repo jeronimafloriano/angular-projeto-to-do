a9cc4bd79a03f13c96696ce77e46efd31db24962
jeronima <jeronima.floriano@totvs.com.br>
2023-04-21 09:40:39 -0300

    Adicionando SCSS dos componentes e definindo como será estruturado no html

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+
+  public taskList: Array<TaskList> = [];
 
 }
